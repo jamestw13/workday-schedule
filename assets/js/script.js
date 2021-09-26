8b75193db6a892476676188bc07ac5c1b0aec955
TJ James <jamestw13@gmail.com>
2021-09-26 15:09:44 -0500

    Created rows for each hour in the workday

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,38 @@
 let events = {};
 const START_HOUR = 9;
 const END_HOUR = 17;
+let date = moment().local();
 
 let loadPage = function () {
 	// Add current date to header
-	let date = moment().local();
-
-	console.log(date);
 	$("#currentDay").text(date.format("dddd, MMMM, Do"));
 
 	// Add Time Blocks
 	events = JSON.parse(localStorage.getItem("events"));
 
+	// Check if event are in localStorage
 	if (!events) {
+		// If not, create an events object
 		let events = {};
+
 		for (let i = START_HOUR; i <= END_HOUR; i++) {
 			let workHour = date.set("hour", i).format("hA");
-			Object.defineProperty(events, workHour, {value: ""});
+			// Object.defineProperty(events, workHour, {value: ""});
+
+			let timeBlock = $("<div>").addClass("row");
+
+			let timeColumn = $("<div>").text(workHour).addClass("col-2 list-group-item");
+			let textColumn = $("<div>").addClass("col-8 list-group-item");
+			textColumn.text("textColumn");
+			let saveButton = $("<div>").addClass("col-2 list-group-item");
+			saveButton.text("saveButton");
+
+			timeBlock.append(timeColumn);
+			timeBlock.append(textColumn);
+			timeBlock.append(saveButton);
+
+			$("#timeBlockList").append(timeBlock);
 		}
-
-		// let timeBlock = $("<li>").addClass("row list-group-item");
-
-		// let timeColumn = $("<div>").text(date.set("hour", i).format("HA")).addClass("col");
-		// let textColumn = $("<div>").addClass("btn-success col");
-		// let saveButton = $("<div>").addClass("btn-primary col");
-
-		// timeBlock.add(timeColumn);
-		// timeBlock.add(textColumn);
-		// timeBlock.add(saveButton);
-		// $("#timeBlockList").add(timeBlock);
-
-		console.log(events);
 	}
 };
 
